e0cc62341134c8afd15373ce2e4efc1ad255f0c3
Gustavo Bueno <gustavo.bueno89@gmail.com>
2018-06-06 20:24:42 +0200

    add navigation with react router

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 
-import Home from './components/Home'
+import Routes from './Routes'
 
-ReactDOM.hydrate(<Home />, document.querySelector('#root'))
+ReactDOM.hydrate(
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
+  document.querySelector('#root'))
